549095d2047c235a545dfac97134ed8f9eb7dd11
Rob Corey <robertbcorey@gmail.com>
2019-05-22 10:52:06 -0400

    Object ORiented

--- src/liars.ts
+++ src/liars.ts
@@ -1,97 +1,159 @@
-import { mockLobby } from './mocks'
+import { mockLobby, mockPlayers } from './mocks'
 interface Player {
   id: number
   name: string
-  hand: []
+}
+interface GamePlayer extends Player {
+  hand: number[]
+}
+
+interface UserSettings {
+  numberOfStartingDice?: number
 }
 interface Settings {
   numberOfStartingDice: number
 }
 
-interface Round {
-  bids: Bid[]
+interface Bid {
+  value: number
+  number: number
+}
+
+interface PlayerBid extends Bid {
+  playerId: Player['id']
 }
 
 class Game {
-  players: Player[] = []
-  rounds: Round[]
-  currentRoundIndex: number
-  currentPlayerIndex: number
-  winners: Player[] = []
-  losers: Player[] = []
+  players: GamePlayer[] = []
+  roundHistory = []
+  bids: PlayerBid[] = []
+  currentPlayerIndex: number = -1
+  winners: GamePlayer[] = []
+  losers: GamePlayer[] = []
+  defaultSettings: Settings = {
+    numberOfStartingDice: 5
+  }
+  settings: Settings
 
-  constructor(public id: number) {}
+  constructor(public id: number, private userSettings: UserSettings) {
+    this.settings = { ...this.defaultSettings, ...userSettings }
+  }
+
+  print() {
+    console.log(JSON.stringify(this, null, 2))
+  }
 
   startGame() {
-    this.rounds = [{ bids: [] }]
-    this.currentRoundIndex = 0
     this.currentPlayerIndex = Math.floor(Math.random() * this.players.length)
+    this.dealDice(this.settings.numberOfStartingDice)
+  }
+
+  private dealDice(numberOfStartingDice: number) {
+    this.players = this.players.map(player => {
+      player.hand = this.generateHand(numberOfStartingDice)
+      return player
+    })
+  }
+
+  private generateHand(numberOfDice: number) {
+    return new Array(numberOfDice).fill(undefined).map(() => Math.floor(Math.random() * 6) + 1)
+  }
+
+  getNumberOfDie(target: number) {
+    return this.players.flatMap(player => player.hand).filter(die => die === target).length
+  }
+
+  private convertPlayerToGamePlayer(player: Player) {
+    return { ...player, hand: [] }
   }
 
   addPlayer(player: Player) {
-    this.players = [...this.players, player]
+    this.players = [...this.players, this.convertPlayerToGamePlayer(player)]
   }
 
   addPlayers(players: Player[]) {
-    this.players = [...this.players, ...players]
+    this.players = [...this.players, ...players.map(this.convertPlayerToGamePlayer)]
   }
 
   removePlayer(playerId: number) {
     this.players = this.players.filter(player => player.id !== playerId)
   }
+
+  getCurrentPlayer() {
+    return this.players[this.currentPlayerIndex]
+  }
+
+  getCurrentBid() {
+    return this.bids[this.bids.length - 1]
+  }
+
+  convertBidToPlayerBid(bid: Bid): PlayerBid {
+    return { ...bid, playerId: this.getCurrentPlayer().id }
+  }
+  makeBid(bid: Bid) {
+    this.bids = [...this.bids, this.convertBidToPlayerBid(bid)]
+  }
+
+  callLiar() {
+    const currentBid = this.bids[this.bids.length - 1]
+    const actualNumberOfDie = this.getNumberOfDie(currentBid.value)
+    if (currentBid.number === actualNumberOfDie) {
+      this.handleSpotOn()
+    }
+    if (currentBid.number > actualNumberOfDie) {
+      this.handleGreaterThan()
+    }
+    if (currentBid.number < actualNumberOfDie) {
+      this.handleLessThan()
+    }
+    this.removeWinners()
+    if (this.isEndGame()) {
+      this.handleEndGame()
+    }
+  }
+
+  handleSpotOn() {
+    this.handleLessThan()
+  }
+
+  handleLessThan() {
+    this.players = this.removeDice(this.getCurrentPlayer().id)
+  }
+
+  handleGreaterThan() {
+    this.players = this.removeDice(this.getCurrentBid().playerId)
+  }
+
+  private removeDice(excludedPlayerId: number): GamePlayer[] {
+    return this.players.map(player => {
+      if (player.id !== excludedPlayerId) {
+        player.hand = this.generateHand(player.hand.length - 1)
+      }
+      return player
+    })
+  }
+
+  removeWinners() {
+    this.winners = [...this.winners, ...this.players.filter(player => player.hand.length === 0)]
+    this.players = [...this.players, ...this.players.filter(player => player.hand.length !== 0)]
+  }
+
+  isEndGame() {
+    return this.players.length === 1
+  }
+
+  handleEndGame() {
+    this.losers = this.players
+    this.players = []
+  }
 }
 
-// const dealDice = (player: Player, numberOfStartingDice: number): GamePlayer => {
-//   const hand = new Array(numberOfStartingDice)
-//     .fill(undefined)
-//     .map(() => Math.floor(Math.random() * 6) + 1)
-//   return {
-//     ...player,
-//     hand
-//   }
-// }
+let game = new Game(1, {})
+game.addPlayers([{ id: 1, name: 'Rob' }, { id: 2, name: 'Tom' }])
+game.startGame()
 
-// const getNumberOfDie = (game: Game, target: number) =>
-//   game.players.flatMap(player => player.hand).filter(die => die === target).length
-
-// const handleBid = (game: Game, bid: Bid): => {
-//   if (game.players[game.currentPlayerIndex].id !== bid.playerId) {
-//     throw new Error('Error: only the current player is allowed to bid')
-//   }
-//   if (bid.type === BidType.liar) {
-//     return handleLiarBid(game, bid)
-//   }
-
-//   if (bid.type === BidType.call) {
-//     return handleCallBid(game, bid)
-//   }
-// }
-
-// const handleCallBid = (game: Game, bid: Bid) => {
-//   return {...game, rounds: [...game.rounds, {}]}
-
-// }
-// const handleLiarBid = (game: Game, bid: Bid) => {}
-
-// const getDefaultSettings = (): Settings => {
-//   return {
-//     numberOfStartingDice: 5
-//   }
-// }
-
-// const startGame = (lobby: Lobby, settings: any = {}): Game => {
-//   let mergedSettings = { ...getDefaultSettings(), settings }
-//   return {
-//     ...lobby,
-//     players: lobby.players.map(player => dealDice(player, mergedSettings.numberOfStartingDice)),
-//     winners: [],
-//     losers: [],
-//     rounds: [{ bids: [] }],
-//     currentRoundIndex: 0,
-//     currentPlayerIndex: Math.floor(Math.random() * lobby.players.length)
-//   }
-// }
+console.log('result: %j', game.getNumberOfDie(3))
 
 // let game = startGame(addPlayers(lobby, players), {})
-let result = {}
-console.log('result: %j', result)
+// let result = {}
+// console.log('result: %j', result)
---
+++ src/mocks.ts
@@ -0,0 +1,6 @@
+export const mockPlayers = [
+  { id: 1, name: 'Rob', record: { wins: 1, losses: 0 } },
+  { id: 2, name: 'Tom', record: { wins: 0, losses: 1 } }
+]
+
+export const mockLobby = { id: 1, players: [] }
