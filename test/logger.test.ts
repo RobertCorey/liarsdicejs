7748fdc12592aae529cd966bb9828b6beaac8ece
Rob Corey <robertbcorey@gmail.com>
2019-06-11 07:57:41 -0400

    blah blah blah

--- test/logger.test.ts
+++ test/logger.test.ts
@@ -1,18 +1,8 @@
-import { Logger, Log } from '../src/logger'
-import { mockLog } from './mocks'
+import { Logger } from '../src/logger'
 
 describe('Logger', () => {
   let logger: Logger
   beforeEach(() => {
     logger = new Logger()
-  })
-  describe('addLog', () => {
-    it('should add the log', () => {
-      logger.addLog(mockLog)
-      expect(logger.logs.length).toBe(1)
-    })
-    it('should convert the Log to a LogWithTimestamp', () => {
-      expect(logger.addLog.length).toBe(1)
-    })
   })
 })
